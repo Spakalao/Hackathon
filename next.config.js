18ac0514cc701185bd6aa77df0292fdee805c8d6
mohamed ben lboukht <mohamedbenlboukh10@gmail.com>
2025-03-14 06:55:05 +0100

    op

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['source.unsplash.com'],
+    domains: ['source.unsplash.com', 'images.unsplash.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'source.unsplash.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
         pathname: '**',
       },
     ],
